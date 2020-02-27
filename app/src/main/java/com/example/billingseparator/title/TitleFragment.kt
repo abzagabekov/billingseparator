0c7f16287e9443c912aa556228c89d766b7f4284
abzal <abz@mail.com>
2020-02-27 15:22:49 +0600

    clean up title fragment

--- app/src/main/java/com/example/billingseparator/title/TitleFragment.kt
+++ app/src/main/java/com/example/billingseparator/title/TitleFragment.kt
@@ -20,45 +20,18 @@ import kotlinx.coroutines.*
 
 class TitleFragment : Fragment() {
 
-    private val fragmentJob = Job()
-    private val uiScope = CoroutineScope(Dispatchers.Main + fragmentJob)
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val binding: TitleFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.title_fragment, container, false)
-        val application = requireNotNull(this.activity).application
-        val billDatabase = BillDatabase.getInstance(application).billDatabaseDao
 
         binding.btnStart.setOnClickListener {
-
-            uiScope.launch {
-                val firstBill = getBill(1, billDatabase)
-                if (firstBill == null) {
-                    insert(billDatabase, Bill(billName = "FirstBill"))
-                }
-                findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToBillsFragment())
-            }
+            findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToBillsFragment())
         }
 
         return binding.root
     }
-
-    private suspend fun insert(database: BillDatabaseDao, bill: Bill) {
-        withContext(Dispatchers.IO) {
-            database.insert(bill)
-        }
-    }
-
-    private suspend fun getBill(key: Long, billDatabaseDao: BillDatabaseDao): Bill? {
-        var res: Bill? = null
-        withContext(Dispatchers.IO) {
-            res = billDatabaseDao.get(key)
-        }
-        return res
-    }
-
 
 }
