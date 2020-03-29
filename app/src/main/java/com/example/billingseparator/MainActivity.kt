d13efe7c97e124ee0291ad537acb4bb487758232
abz <abzagabekov@gmail.com>
2020-03-29 19:32:28 +0600

    adding share (incomplete)

--- app/src/main/java/com/example/billingseparator/MainActivity.kt
+++ app/src/main/java/com/example/billingseparator/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.billingseparator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.VmPolicy
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 
+
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_EXTERNAL_STORAGE = 1
+    private val PERMISSIONS_STORAGE = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,10 +26,32 @@ class MainActivity : AppCompatActivity() {
 
         val navController = this.findNavController(R.id.nav_host_fragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+        val builder = VmPolicy.Builder()
+        StrictMode.setVmPolicy(builder.build())
+
+        verifyStoragePermissions(this)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.nav_host_fragment)
         return navController.navigateUp()
+    }
+
+    private fun verifyStoragePermissions(activity: Activity?) {
+        // Check if we have write permission
+        val permission =
+            ActivityCompat.checkSelfPermission(
+                activity!!,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                activity,
+                PERMISSIONS_STORAGE,
+                REQUEST_EXTERNAL_STORAGE
+            )
+        }
     }
 }
--- app/src/main/java/com/example/billingseparator/result/ResultFragment.kt
+++ app/src/main/java/com/example/billingseparator/result/ResultFragment.kt
@@ -1,22 +1,36 @@
 package com.example.billingseparator.result
 
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.os.Environment
+import android.os.Handler
+import android.view.*
 import android.widget.TableRow
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.ShareActionProvider
+import androidx.core.view.MenuItemCompat
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-
 import com.example.billingseparator.R
 import com.example.billingseparator.database.BillDatabase
 import com.example.billingseparator.databinding.ResultFragmentBinding
+import kotlinx.android.synthetic.main.result_fragment.*
 import kotlinx.android.synthetic.main.sample_table_row.view.*
+import java.io.File
+import java.io.FileOutputStream
 
 class ResultFragment : Fragment() {
+
+    private lateinit var menuItem: MenuItem
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,6 +47,7 @@ class ResultFragment : Fragment() {
 
         showResults(resultViewModel, inflater, container, binding)
 
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -57,8 +72,69 @@ class ResultFragment : Fragment() {
             newRow.tv_price_item.text = "${getString(R.string.sumary)} ${summary.toString()}"
             newRow.tv_product_item.text = ""
             binding.tableResult.addView(newRow)
+
+            (MenuItemCompat.getActionProvider(menuItem) as ShareActionProvider).setShareIntent(getShareIntent())
         })
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_result, menu)
+        menuItem = menu.findItem(R.id.menu_item_share)
+
+    }
+
+    private fun getShareIntent(): Intent {
+
+        table_result.isDrawingCacheEnabled = true
+        val bitmap = table_result.drawingCache
+        val appDir = File(Environment.getExternalStorageDirectory().absolutePath + "/" + activity?.packageName)
+        if (!appDir.exists()) {
+            appDir.mkdir()
+        }
+        val cachePath = File(appDir, "image_bill.jpg")
+        try {
+            cachePath.createNewFile()
+            val fos = FileOutputStream(cachePath)
+            bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+            fos.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        val shareIntent = Intent()
+        shareIntent.action = Intent.ACTION_SEND
+        shareIntent.type = "image/*"
+        shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(cachePath))
+        return shareIntent
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getBitmapFromView(view: View, activity: Activity, callback: (Bitmap) -> Unit) {
+        activity.window?.let { window ->
+            val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+            val locationOfViewInWindow = IntArray(2)
+            view.getLocationInWindow(locationOfViewInWindow)
+            try {
+                PixelCopy.request(window, Rect(locationOfViewInWindow[0], locationOfViewInWindow[1], locationOfViewInWindow[0] + view.width, locationOfViewInWindow[1] + view.height), bitmap, { copyResult ->
+                    if (copyResult == PixelCopy.SUCCESS) {
+                        callback(bitmap)
+                    }
+                    // possible to handle other result codes ...
+                }, Handler())
+            } catch (e: IllegalArgumentException) {
+                // PixelCopy may throw IllegalArgumentException, make sure to handle it
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun loadBitmapFromView(v: View, width: Int, height: Int): Bitmap? {
+        val b = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val c = Canvas(b)
+        v.layout(0, 0, v.layoutParams.width, v.layoutParams.height)
+        v.draw(c)
+        return b
     }
 
 
